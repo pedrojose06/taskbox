1c5419272a2f74a1421a1d631de0bd10f1fe581a
Pedro Jose Moraes <pjmoraes@americastg.com>
2025-07-14 15:09:43 -0300

    feat: includ tests to storybook

--- src/components/InboxScreen.stories.tsx
+++ src/components/InboxScreen.stories.tsx
@@ -5,11 +5,18 @@ import InboxScreen from './InboxScreen';
 
 import store from '../lib/store';
 
- import { http, HttpResponse } from 'msw';
+import { http, HttpResponse } from 'msw';
 
- import { MockedState } from './TaskList.stories';
+import { MockedState } from './TaskList.stories';
 
 import { Provider } from 'react-redux';
+
+ import {
+  fireEvent,
+  waitFor,
+  within,
+  waitForElementToBeRemoved
+ } from '@storybook/test';
 
 const meta = {
   component: InboxScreen,
@@ -22,27 +29,39 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
- parameters: {
-   msw: {
-     handlers: [
-       http.get('https://jsonplaceholder.typicode.com/todos?userId=1', () => {
-         return HttpResponse.json(MockedState.tasks);
-       }),
-     ],
-   },
+  parameters: {
+    msw: {
+      handlers: [
+        http.get('https://jsonplaceholder.typicode.com/todos?userId=1', () => {
+          return HttpResponse.json(MockedState.tasks);
+        }),
+      ],
+    },
+  },
+ play: async ({ canvasElement }) => {
+   const canvas = within(canvasElement);
+   // Waits for the component to transition from the loading state
+   await waitForElementToBeRemoved(await canvas.findByTestId('loading'));
+   // Waits for the component to be updated based on the store
+   await waitFor(async () => {
+     // Simulates pinning the first task
+     await fireEvent.click(canvas.getByLabelText('pinTask-1'));
+     // Simulates pinning the third task
+     await fireEvent.click(canvas.getByLabelText('pinTask-3'));
+   });
  },
 };
 
 export const Error: Story = {
- parameters: {
-   msw: {
-     handlers: [
-       http.get('https://jsonplaceholder.typicode.com/todos?userId=1', () => {
-         return new HttpResponse(null, {
-           status: 403,
-         });
-       }),
-     ],
-   },
- },
+  parameters: {
+    msw: {
+      handlers: [
+        http.get('https://jsonplaceholder.typicode.com/todos?userId=1', () => {
+          return new HttpResponse(null, {
+            status: 403,
+          });
+        }),
+      ],
+    },
+  },
 };
