d0a8fa17761724df3cfd1f4207bac6d40fbfadfe
Pedro Jose Moraes <pjmoraes@americastg.com>
2025-07-14 11:06:19 -0300

    feat: add simpla and complex compinent and stories

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,9 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/components/**/*.stories.@(ts|tsx)'],
   staticDirs: ["../public"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-a11y", "@storybook/addon-docs"],
   framework: {
     name: "@storybook/react-vite",
     options: {},
---
+++ src/components/Task.stories.tsx
@@ -0,0 +1,52 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+
+import Task from "./Task";
+import { fn } from "storybook/test";
+
+export const ActionsData = {
+    onArchiveTask: fn(),
+    onPinTask: fn(),    
+}
+
+const meta = {
+    component: Task,
+    title: "Task",
+    tags: ["autodocs"],
+    excludeStories: /.*Data$/,
+    args: {
+        ...ActionsData,
+    }
+} satisfies Meta<typeof Task>;
+
+export default meta;
+type Story = StoryObj<typeof Task>;
+
+export const Default: Story = {
+    args: {
+        task: {
+            id: "1",
+            title: "My Task",
+            state: "TASK_INBOX",
+        },
+    },
+};
+
+export const Pinned: Story = {
+    args: {
+        task: {
+            id: "2",
+            title: "My Pinned Task",
+            state: "TASK_PINNED",
+        },
+    },
+};
+
+export const Archived: Story = {
+    args: {
+        task: {
+            id: "3",
+            title: "My Archived Task",
+            state: "TASK_ARCHIVED",
+        },
+    },
+};
