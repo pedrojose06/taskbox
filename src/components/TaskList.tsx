d9b058ccac8a68a5ca7a6ae2b675e61b93ff6259
Pedro Jose Moraes <pjmoraes@americastg.com>
2025-07-14 11:50:09 -0300

    feat: fetchdata from redux

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,70 +1,79 @@
 
-import type { TaskData } from '../types';
-
 import Task from './Task';
 
-type TaskListProps = {
-  /** Checks if it's in loading state */
-  loading?: boolean;
-  /** The list of tasks */
-  tasks: TaskData[];
-  /** Event to change the task to pinned */
-  onPinTask: (id: string) => void;
-  /** Event to change the task to archived */
-  onArchiveTask: (id: string) => void;
-};
+import { useDispatch, useSelector } from 'react-redux';
 
-export default function TaskList({
-  loading = false,
-  tasks,
-  onPinTask,
-  onArchiveTask,
-}: TaskListProps) {
-  const events = {
-    onPinTask,
-    onArchiveTask,
-  };
-  const LoadingRow = (
-    <div className="loading-item">
-      <span className="glow-checkbox" />
-      <span className="glow-text">
-        <span>Loading</span> <span>cool</span> <span>state</span>
-      </span>
-    </div>
-  );
-  if (loading) {
-    return (
-      <div className="list-items" data-testid="loading" key={"loading"}>
-        {LoadingRow}
-        {LoadingRow}
-        {LoadingRow}
-        {LoadingRow}
-        {LoadingRow}
-        {LoadingRow}
-      </div>
-    );
-  }
-  if (tasks.length === 0) {
-    return (
-      <div className="list-items" key={"empty"} data-testid="empty">
-        <div className="wrapper-message">
-          <span className="icon-check" />
-          <p className="title-message">You have no tasks</p>
-          <p className="subtitle-message">Sit back and relax</p>
+import { updateTaskState, RootState, AppDispatch } from '../lib/store';
+
+
+
+export default function TaskList() {
+
+const  tasks = useSelector((state: RootState) => {
+    const tasksInOrder = [
+        ...state.taskbox.tasks.filter((t) => t.state === "TASK_PINNED"),
+        ...state.taskbox.tasks.filter((t) => t.state !== "TASK_PINNED"),
+
+    ]
+        const filteredTasks = tasksInOrder.filter((t) => t.state === 'TASK_INBOX' || t.state === 'TASK_PINNED');
+        return filteredTasks;
+
+    });
+
+    const status = useSelector((state: RootState) => state.taskbox.status);
+
+    const dispatch = useDispatch<AppDispatch>();
+
+    const pinTask = (value: string) => {
+        dispatch(updateTaskState({ id: value, newTaskState: 'TASK_PINNED' }));
+    }
+    const archiveTask = (value: string) => {
+        // We're dispatching the Archive event back to our store
+        dispatch(updateTaskState({ id: value, newTaskState: 'TASK_ARCHIVED' }));
+    };
+
+    const LoadingRow = (
+        <div className="loading-item">
+        <span className="glow-checkbox" />
+        <span className="glow-text">
+            <span>Loading</span> <span>cool</span> <span>state</span>
+        </span>
         </div>
-      </div>
     );
-  }
+    if (status === "loading") {
+        return (
+        <div className="list-items" data-testid="loading" key="loading">
+            {LoadingRow}
+            {LoadingRow}
+            {LoadingRow}
+            {LoadingRow}
+            {LoadingRow}
+            {LoadingRow}
+        </div>
+        );
+    }
+    if (tasks.length === 0) {
+        return (
+        <div className="list-items" key="empty" data-testid="empty">
+            <div className="wrapper-message">
+            <span className="icon-check" />
+            <p className="title-message">You have no tasks</p>
+            <p className="subtitle-message">Sit back and relax</p>
+            </div>
+        </div>
+        );
+    }
 
-  const tasksInOrder = [
-    ...tasks.filter((t) => t.state === "TASK_PINNED"),
-    ...tasks.filter((t) => t.state !== "TASK_PINNED"),
-  ];
-  return (
-    <div className="list-items">
-      {tasksInOrder.map((task) => (
-        <Task key={task.id} task={task} {...events} />
-      ))}
-    </div>
-  );
+    return (
+        <div className="list-items" data-testid="success" key="success">
+        {tasks.map((task) => (
+            <Task
+            key={task.id}
+            task={task}
+            onPinTask={pinTask}
+            onArchiveTask={archiveTask}
+            />
+        ))}
+        </div>
+    );
 }
